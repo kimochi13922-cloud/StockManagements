b0fbfeb25ffc851d0a128a6e7be61f937931bd08
kimochi13922-cloud <kimochi13922@gmail.com>
2026-02-12 19:40:12 +0700

    Add files via upload

---
+++ stockmanage/src/main/java/com/proj/stockmanage/StockmanageApplication.java
@@ -0,0 +1,13 @@
+package com.proj.stockmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmanageApplication.class, args);
+	}
+
+}
