5a6cda383a8588d22d48ef5b03ede41b5f1c644d
kimochi13922-cloud <kimochi13922@gmail.com>
2026-02-22 18:08:08 +0700

    Add files via upload

---
+++ stockmanageV3/src/main/java/com/proj/stockmanage/clothes.java
@@ -0,0 +1,17 @@
+package com.proj.stockmanage;
+
+public class clothes extends Product {
+    double taxRate = 0.12;
+
+    public clothes(String name, int quantity, String id, double price, String description ) {
+        super(name, quantity, id, price, description);
+    }
+
+    public double calculateTax() {
+        return getPrice()*getQuantity() * taxRate; // Assuming a tax rate of 12% for clothes
+    }
+    
+    public double getTaxRate() {
+        return taxRate;
+    }
+}
